265486512f457518b54164d7a6b8a2cf8bfa8b75
khatrisaugat <khatri.saugat55@gmail.com>
2020-09-21 15:16:29 +0545

    Navbar added and copy functionality

--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+
 import './Palette.css';
 
 export default class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 600 };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({ level });
+
+        // console.log(level);
+    }
     render() {
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name} />
+        const { level } = this.state;
+        const colorBoxes = this.props.palette.colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />
         ));
         return (
             <div className="Palette">
+
                 {/* <h1>Palette</h1> */}
                 {/* Navbar */}
+                <Navbar level={level} changeLevel={this.changeLevel} />
                 <div className="Palette-colors">
                     {/* bunch of color boxes */}
                     {colorBoxes}
