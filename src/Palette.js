e643343cc4f699a0228d157db9e36c17ac45a8f9
khatrisaugat <khatri.saugat55@gmail.com>
2020-09-22 16:43:22 +0545

    using material ui

--- src/Palette.js
+++ src/Palette.js
@@ -7,25 +7,29 @@ import './Palette.css';
 export default class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 600 };
+        this.state = { level: 600, format: "hex" };
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
     changeLevel(level) {
         this.setState({ level });
 
         // console.log(level);
     }
+    changeFormat(value) {
+        this.setState({ format: value });
+    }
     render() {
-        const { level } = this.state;
+        const { level, format } = this.state;
         const colorBoxes = this.props.palette.colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} />
         ));
         return (
             <div className="Palette">
 
                 {/* <h1>Palette</h1> */}
                 {/* Navbar */}
-                <Navbar level={level} changeLevel={this.changeLevel} />
+                <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
                 <div className="Palette-colors">
                     {/* bunch of color boxes */}
                     {colorBoxes}
